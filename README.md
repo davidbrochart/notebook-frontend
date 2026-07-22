b6e1e7efeb5192666434a5eb11578048e19f7ce7
David Brochart <david.brochart@gmail.com>
2026-07-22 15:41:11 +0200

    Release v7.6.1

--- README.md
+++ README.md
@@ -2,15 +2,8 @@
 
 A Python package distributing Notebook's static assets only, with no Python dependency.
 
+To publish, make sure you have `toml` and `hatch` installed, and run e.g.:
+
 ```bash
-git clean -fdx
-curl --output notebook-7.6.0-py3-none-any.whl https://files.pythonhosted.org/packages/93/d1/e617c40db57ff40e75f43a7d4d1c305e3a54c053ab5cb0534a6c314664f9/notebook-7.6.0-py3-none-any.whl
-unzip notebook-7.6.0-py3-none-any.whl
-mkdir -p share
-cp -r notebook-7.6.0.data/data/share/jupyter share/
-cp -r notebook/static src/notebook_frontend/
-cp -r notebook/templates src/notebook_frontend/
-# update version in `pyproject.toml`, and also `jupyterlab-js` version
-hatch build
-hatch publish
+python publish.py 7.6.1 4.6.1  # notebook and jupyterlab versions
 ```
---
+++ publish.py
@@ -0,0 +1,29 @@
+import sys
+from subprocess import check_output
+
+import toml
+
+
+version = sys.argv[1]
+jupyterlab_version = sys.argv[2]
+
+check_output("git clean -fdx".split())
+check_output(f"curl -L --output notebook-{version}-py3-none-any.whl https://pypi.org/packages/py3/n/notebook/notebook-{version}-py3-none-any.whl".split())
+check_output(f"unzip notebook-{version}-py3-none-any.whl".split())
+check_output("mkdir -p share".split())
+check_output(f"cp -r notebook-{version}.data/data/share/jupyter share/".split())
+check_output("cp -r notebook/static src/notebook_frontend/".split())
+check_output("cp -r notebook/templates src/notebook_frontend/".split())
+
+with open("pyproject.toml", "rt") as f:
+    data = toml.load(f)
+
+with open("pyproject.toml", "wt") as f:
+    data["project"]["version"] = version
+    data["dependencies"] = [f"jupyterlab-js=={jupyterlab_version}"]
+    toml.dump(data, f)
+
+check_output("hatch build".split())
+check_output("hatch publish".split())
+check_output(["git", "commit", "-am", f"Release v{version}"])
+check_output("git push".split())
